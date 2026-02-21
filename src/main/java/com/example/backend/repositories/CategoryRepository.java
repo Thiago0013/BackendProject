8150240b6a7143786d34fb8b45fa697ee4c095e7
Thiago0013 <ianthigo13@gmail.com>
2026-02-21 13:26:19 -0300

    feat(Category): removendo categoria

--- src/main/java/com/example/backend/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repositories;
-
-import com.example.backend.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
