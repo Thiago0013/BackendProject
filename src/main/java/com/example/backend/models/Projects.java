204703fbb02edcc64bcfce10ab98e9a753eead41
Thiago0013 <ianthigo13@gmail.com>
2026-02-22 13:31:04 -0300

    feat(projects): deixando o texto longo

--- src/main/java/com/example/backend/models/Projects.java
+++ src/main/java/com/example/backend/models/Projects.java
@@ -28,7 +28,7 @@ public class Projects {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
